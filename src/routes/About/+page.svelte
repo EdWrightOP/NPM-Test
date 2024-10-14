d9d61bf29761150f223faf9660b2ce0574e126ac
Edward Wright (1000126272) <WRIGEG1@student.op.ac.nz>
2024-10-15 11:49:04 +1300

    created 3 pages

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,9 +1,10 @@
 <script>
-
+    import Header from "../../lib/Header.svelte";
+    import Footer from "../../lib/Footer.svelte";
 </script>
-
+<Header headingTitle="About Page"/>
 <p>sfsdfsdfsdf</p>
-
+<Footer/>
 <style>
-    
+
 </style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+import Header from "../../lib/Header.svelte";
+    import Footer from "../../lib/Footer.svelte";
+</script>
+<Header headingTitle = "Contact"/>
+<section><ul>
+    <li>
+        <label>Name: </label><input type="text" name="Name" id="name">
+    </li>
+    <li>
+        <label>Email: </label><input type="email" name="email" id="email">
+    </li>
+    <li>
+        <label>Your Message: </label><input type="text" name="Message" id="message">
+    </li>
+</ul></section>
+<Footer />
+<style></style>
